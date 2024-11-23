df2f2c3d73fca73ffdcef1274bf9e55165c02832
Bernnardoo <bernnardoolivjobs@icloud.com>
2024-11-22 22:05:42 -0300

    Update page.js

--- page.js
+++ page.js
@@ -1,34 +1,4 @@
-[21:33, 22/11/2024] Naomi: "use client";
-import React, { useEffect, useState } from "react";
-import styles from "./page.module.css";
-import VitrineAnimesCard from './components/VitrineAnimesCard.jsx'
-export default function Home() {
-  const [animes, setAnimes] = useState([]); 
-
-  useEffect(() => {
-    async function fetchAnimes() {
-      try {
-        const res = await fetch("https://api.jikan.moe/v4/seasons/2021/spring?sfw");
-        const dados = await res.json(); 
-        setAnimes(dados); 
-        console.log(dados);
-      } catch (err) {
-        console.error("Failed to fetch heroes:", err);
-      }
-    }
-
-    fetchAnimes();
-  }, []);
-
-  return (
-      <div className={styles.container}>
-          <VitrineAnimeCard animes={animes.data}>
-              
-          </VitrineAnimesCard>
-    </div>
-  );
-}
-[21:33, 22/11/2024] Naomi: "use client";
+"use client";
 import React, { useEffect, useState } from "react";
 import styles from "./page.module.css";
 import VitrineAnimesCard from './components/VitrineAnimesCard.jsx'
